37b60cda81596b9faab586662c11b8f29dea08c2
Shakshi kumari maurya <shakshikumarimaurya@gmail.com>
2026-04-06 04:15:15 +0530

    final testing

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
           <div className="flex items-center gap-2">
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
-            <p>CI/ CD working.............yeaaahahhhhhh</p>
+           
           </div>
           <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
             Latest Arrivals
